881a5b18473fe8cb607789ccde4d93c27ca26ac7
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-17 22:53:32 +0630

    Delete src/types/src/hooks/src/services/rideService.ts

--- src/types/src/hooks/src/services/rideService.ts
+++
@@ -1,8 +0,0 @@
-import { rtdb } from '@/lib/firebase';
-import { ref, push, set } from 'firebase/database';
-
-export const createRideRequest = async (rideData: any) => {
-  const ridesRef = ref(rtdb, 'rides');
-  const newRideRef = push(ridesRef);
-  return set(newRideRef, { ...rideData, id: newRideRef.key });
-};
