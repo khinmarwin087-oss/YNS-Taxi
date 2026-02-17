46e460b05820948d9ae0a0451c22c1d47b999686
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-17 21:21:03 +0630

    Create rideService.ts

---
+++ src/types/src/hooks/src/services/rideService.ts
@@ -0,0 +1,8 @@
+import { rtdb } from '@/lib/firebase';
+import { ref, push, set } from 'firebase/database';
+
+export const createRideRequest = async (rideData: any) => {
+  const ridesRef = ref(rtdb, 'rides');
+  const newRideRef = push(ridesRef);
+  return set(newRideRef, { ...rideData, id: newRideRef.key });
+};
