2b928691e9beeb482646caf1e074d1dfd87d04dc
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-17 22:47:46 +0630

    Create GoogleMap.tsx

---
+++ src/components/map/GoogleMap.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const GoogleMap = () => {
+  return (
+    <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+       <span className="text-gray-500 font-medium">Google Maps Interface</span>
+    </div>
+  );
+};
+
+export default GoogleMap;
