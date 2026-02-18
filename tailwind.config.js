4d4395918d6c4bfba33f59299c016020f8cfa352
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-18 16:14:01 +0630

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // ဒီလမ်းကြောင်းက src အောက်က အကုန်လုံးကို ဖတ်ပေးမှာပါ
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
